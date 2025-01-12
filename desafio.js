df49b0954e7f353f987f510fd9090ba123282e29
Minf-afk <msantana.inf@gmail.com>
2025-01-11 23:47:24 -0300

    Ajustando

---
+++ desafio.js
@@ -0,0 +1,14 @@
+let posicaoInicial = 10
+let numeroDePassos = 27
+
+
+let totalDePassos = posicaoInicial + numeroDePassos
+
+console.log("O herói deu um total de " + totalDePassos + " passos!")
+
+if(totalDePassos < 25) {
+	console.log("Ainda estamos a uma longa jornada de encontrar o Grande Dragão!")
+}
+else{
+	console.log("Prepare-se para o Desafio Final")
+}
