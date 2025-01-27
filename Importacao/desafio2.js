d654673562a5cbf2078dbd91ab2158c1155d5e43
Minf-afk <msantana.inf@gmail.com>
2025-01-27 20:59:09 -0300

    Muitas dificuldades

--- Importacao/desafio2.js
+++ Importacao/desafio2.js
@@ -2,23 +2,21 @@ const { gets, print } = require("./funcoes-auxiliares2.js");
 
 const n = gets();
 
-let maiorPar = 0;
-let menorImpar = 0;
+let maiorPar = null;
+let menorImpar = null;
 
 for (let i = 0; i < n; i++) {
   const numero = gets();
   if (numero % 2 === 0) {
-    if (numero > maiorPar) {
+    if (numero > maiorPar || maiorPar === null) {
       maiorPar = numero;
     }
   } else {
-    if (menorImpar === 0) {
+    if (menorImpar === null || numero < menorImpar) {
       menorImpar = numero;
-    } else if (numero < menorImpar) {
-      menorImpar = numero;
-    }
+    } 
   }
 }
 
-print(maiorPar);
-print(menorImpar);
+print(`O mairo número par é ${maiorPar}`);
+print(`O menor número impar é ${menorImpar}`);
---
+++ Importacao/desafio3.js
@@ -0,0 +1,29 @@
+const { gets, print } = require("./funcoes-auxiliares3.js");
+
+const valorSalario = gets();
+const beneficio = gets();
+
+function calcularDesconto(valor, desconto) {
+  return valor * (desconto / 100);
+}
+
+function pegarAliquota(salario) {
+  if (salario >= 0 && salario <= 1100) {
+    return 5;
+}else if (salario >= 1101 && salario <= 2500) {
+    return 10;   
+}else {
+    return 15;
+}
+
+
+}
+
+const aliquotaImposto = pegarAliquota(valorSalario);
+
+const valorImposto = calcularDesconto(valorSalario, aliquotaImposto);
+
+const valorATransferir = valorSalario - valorImposto + beneficio;
+
+print(valorATransferir); 
+
