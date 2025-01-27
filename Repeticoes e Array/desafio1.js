fb9f6bb4efebd3bd73e6d7fee78e720bb683614c
Minf-afk <msantana.inf@gmail.com>
2025-01-27 11:35:28 -0300

    Repeticoes

---
+++ Repeticoes e Array/desafio1.js
@@ -0,0 +1,5 @@
+const tabuada = 60;
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`${tabuada} x ${i} = ${tabuada * i}`);
+}
---
+++ Repeticoes e Array/desafio2.js
@@ -0,0 +1,7 @@
+const apenasNumerosPares = [0,1,2,3,4,5,6,7,8]
+
+for (let i = 0; i < apenasNumerosPares.length; i++) {
+  if (apenasNumerosPares[i] % 2 === 0) {
+    console.log(apenasNumerosPares[i]);
+  }
+}
